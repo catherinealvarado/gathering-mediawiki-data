7a71d62fa8f3a9a465d1d15de8dbaf390d5c95c6
calvarado <calvarado@wesleyan.edu>
2016-09-28 17:52:42 -0400

    created a function to clean the string

--- gather.js
+++ gather.js
@@ -1,7 +1,7 @@
 var https = require('https');
 
 
-var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=&titles=Arrest%20and%20assassination%20of%20Ngo%20Dinh%20Diem";//Stack%20Overflow
+var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=&titles=1960%20South%20Vietnamese%20coup%20attempt";//Stack%20Overflow
 //https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=&titles=1689%20Boston%20revolt
 
 //might want to remove this:
@@ -20,10 +20,35 @@ https.get(url, (res) => {
     data = JSON.parse(data)
     var pageid = Object.keys(data.query.pages)[0];
     var content = data.query.pages[pageid].extract
-    // var content = content.replace(/(\r\n|\n|\r)/gm, '')//removing new lines
-    // content = content.replace(/ *\=[^)]*\= */g, "")
-    var content = content.split('== See also ==')[0]
-    var content = content.split('== Notes ==')[0]
+
+    function cleanString(str){
+      str = str.replace(/ *\([^)]*\) */g, "") //remove all sentences with parenthesis [FUTURE]
+              .replace(/ *\[[^)]*\] */g, " ") //remove all sentences with [] [FUTURE]
+              .split('== See also ==')[0]
+              .split('== Notes ==')[0]
+              .replace(/ *\=.*\= */gi, "")
+              // .replace(/(\r\n|\n|\r)/gm, '') //remove single words ////
+
+              //do this once everything cleaned
+              // var content = content.replace(/(\r\n|\n|\r)/gm, '')//removing new lines
+      return str;
+    }
+    content = cleanString(content)
     console.log(content)
   });
 })
+
+
+// git status
+// stage for commit: git add . (or specific names)
+//  git commit -m ""
+//  git push
+
+
+
+
+// new RegExp("\\n")
+//
+// or simply:
+//
+// /\n/
